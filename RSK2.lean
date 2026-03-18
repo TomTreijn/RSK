c666148efe87ba40424df7dc8fa6743b232f1791
Tom Weerheijm <tomweerheijm@proton.me>
2026-03-18 13:04:59 +0100

    proved one branch of bmpshft_row_bi

--- RSK2.lean
+++ RSK2.lean
@@ -150,6 +150,7 @@ theorem bmpshft_row_bi : Function.Bijective bmpshft_row := by
   have is_inv : Function.LeftInverse bmpshft_row_inv bmpshft_row ∧
         Function.RightInverse bmpshft_row_inv bmpshft_row := by
     constructor
+    -- Left inverse
     · refine Function.leftInverse_iff_comp.mpr ?_
       refine Eq.symm (funext ?_)
       intro var
@@ -157,12 +158,118 @@ theorem bmpshft_row_bi : Function.Bijective bmpshft_row := by
       dsimp only [id_eq, Function.comp_apply]
       rw[bmpshft_row]
       split
-      · case h_1 hi =>
+      -- If nothing is bumped out
+      · case h_1 =>
           rw[bmpshft_row_inv]
           grind
-      · case h_2 j hi =>
+      -- If something is bumped out
+      · case h_2 _ j h_some_j _ _ _ =>
           rw[bmpshft_row_inv]
-          sorry
+          have h_eq_some := List.findIdx?_eq_some_iff_getElem.mp (Eq.symm h_some_j)
+          rcases h_eq_some with ⟨hj_lt_len, hrowj_gt, hleft_row_lt⟩
+          rw[decide_eq_true_iff] at hrowj_gt
+          have hleft_lt_k : ∀ (i₂ : ℕ) (_ : i₂ < j), row[i₂] ≤ k := by
+            intro i₂ hij
+            have h_lt := hleft_row_lt i₂ hij
+            rw[Not, decide_eq_true_eq] at h_lt
+            exact Nat.le_of_not_lt h_lt
+          -- Proof that bmpshft_row and bmpshft_row_inv manipulate the same element
+          have set_set : (List.findIdx (· > row[j])) (row.set j k) - 1 = j := by
+            have hj := (List.findIdx?_eq_some_iff_findIdx_eq.mp (Eq.symm h_some_j)).right
+            refine Eq.symm (Nat.eq_sub_of_add_eq' ?_)
+            apply Eq.symm
+            -- If bmpshft_row manipulated the last element
+            -- We have to prove that row' is less than k
+            if h_succj : 1 + j = (row.set j k).length then
+              rw [h_succj, List.findIdx_eq_length_of_false]
+              intro x hx
+              rw [decide_eq_false_iff_not]
+              have ⟨i, hi_lt_len, hx_getElem⟩ := List.mem_iff_getElem.mp hx
+              if hi_eq_j : i = j then
+                simp_rw[←hx_getElem, hi_eq_j, List.getElem_set_self, Nat.not_gt_eq]
+                exact Nat.le_of_succ_le hrowj_gt
+              else
+                have hi_lt_j : i < j := by
+                  rw [←h_succj] at hi_lt_len
+                  have hi_le_j : i ≤ j := Nat.lt_one_add_iff.mp hi_lt_len
+                  exact Nat.lt_of_le_of_ne hi_le_j hi_eq_j
+                rw [List.getElem_set_of_ne (Ne.symm (Nat.ne_of_lt hi_lt_j)) k hi_lt_len] at hx_getElem
+                have hx_le_k : x ≤ k := by
+                  have hleft := hleft_lt_k i hi_lt_j
+                  rw[hx_getElem] at hleft
+                  exact hleft
+                omega
+            -- If bmpshft_row manipulated another element at j
+            -- We have to prove that row'[i] ≤ k for row[i] ≤ j, and greater than k for j+1
+            else
+              rw [List.findIdx_eq]
+              constructor
+              · rw[decide_eq_true_eq]
+                rw[List.getElem_set_ne]
+                · sorry
+                · rw[Nat.add_comm]
+                  exact Nat.ne_add_one j
+              · grind
+              · rw [List.length_set]
+                rw [List.length_set] at h_succj
+                have hsuccj_le_len : j + 1 ≤ row.length := Order.add_one_le_iff.mpr hj_lt_len
+                rw[Nat.add_comm] at hsuccj_le_len
+                exact Nat.lt_of_le_of_ne hsuccj_le_len h_succj
+          -- Using the fact that both manipulate the same data, the rest follows.
+          simp_rw [set_set]
+          rw[List.getElem_set_self]
+          have rows_eq : (row.set j k).set j row[j] = row := by
+            apply List.ext_getElem
+            · repeat rw [List.length_set]
+            · intro i h₁ h₂
+              if h_i : i = j then
+                simp_rw[h_i]
+                rw [List.getElem_set_self]
+              else
+                have h_j_neq_i : j ≠ i := by exact Ne.symm (Ne.intro h_i)
+                repeat rw [List.getElem_set_ne (h_j_neq_i)]
+          simp_rw[rows_eq]
+    · refine Function.rightInverse_iff_comp.mpr ?_
+      refine Eq.symm (funext ?_)
+      intro ⟨row', h_ord', k', h_notnil', h_leq'⟩
+      rw [Function.comp_apply]
+      rw [bmpshft_row_inv.eq_def]
+      simp only [id_eq, gt_iff_lt]
+      split
+      · case h_1 k' _ _ =>
+        rw [bmpshft_row]
+        have none_find_none : List.findIdx? (fun x ↦ decide (x > row'.getLast h_notnil')) row'.dropLast = none := by
+          refine List.findIdx?_eq_none_iff.mpr ?_
+          intro x hx_in_row'
+          have ⟨i, hi_lt_len, hx_getElem⟩ := List.mem_iff_getElem.mp hx_in_row'
+          rw[←List.getElem_length_sub_one_eq_getLast]
+          · rw[List.getElem_dropLast hi_lt_len] at hx_getElem
+            rw[←hx_getElem]
+            rw [List.length_dropLast] at hi_lt_len
+            rw[decide_eq_false_iff_not]
+            rw[Nat.not_gt_eq]
+            have row_len_lt : row'.length - 1 < row'.length := by
+              refine Nat.sub_one_lt ?_
+              exact Nat.ne_zero_of_lt (Nat.add_lt_of_lt_sub hi_lt_len)
+            exact ord_ord.mp h_ord' i (row'.length - 1) hi_lt_len row_len_lt
+        -- Should be replaced with a variant of rw[k'_find_k]
+        split
+        · case h_1 =>
+          simp only [bmpshft_row_out.mk.injEq, and_true]
+          exact Eq.symm (List.dropLast_concat_getLast h_notnil')
+        · case h_2 find_eq_some _ _=>
+            rw [none_find_none] at find_eq_some
+            contradiction
+      · case h_2 k' _=>
+        let j' := (List.findIdx (fun x ↦ decide (k' < x)) row' - 1)
+        have hj'_eq : (List.findIdx (fun x ↦ decide (k' < x)) row' - 1) = j' := by rfl
+        simp_rw [hj'_eq]
+        rw [bmpshft_row]
+        simp only
+        have j_find_j : List.findIdx? (fun x ↦ decide (x > row'[j'])) (row'.set j' k') = some j := by
+  exact Exists.intro bmpshft_row_inv is_inv
+
+example (a b : Nat): (a < b) = (b > a) := by exact?
 
     · refine Function.leftInverse_iff_comp.mpr ?_
       refine Eq.symm (funext ?_)
